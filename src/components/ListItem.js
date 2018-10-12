bcf6d4d8cd0a0a6be1006db99e400730858b1a88
NewtonJ <newtonjoaquim12@gmail.com>
2018-10-11 22:55:23 -0300

    Adicionadas animações

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import {Text, TouchableWithoutFeedback, View} from 'react-native';
+import {Text, TouchableWithoutFeedback, View, LayoutAnimation, NativeModules} from 'react-native';
 import {connect} from 'react-redux'
 
 import {CardSection} from './common';
 import * as actions from '../actions';
 
+const { UIManager } = NativeModules
+UIManager.setLayoutAnimationEnabledExperimental
+    && UIManager.setLayoutAnimationEnabledExperimental(true)
+
 class ListItem extends React.Component{
     
-    renderDescription(){
-        const {library, selectedLibraryId} = this.props
+    componentWillUpdate(){
+        LayoutAnimation.spring();
+    }
 
-        if(library.item.id == selectedLibraryId){
+    renderDescription(){
+        const {library, expanded} = this.props
+
+        if(expanded){
             return(
-                <Text>{library.item.description}</Text>
+                <CardSection>
+                    <Text style={{flex:1}}>{library.item.description}</Text>
+                </CardSection>
             );
         }
     }
@@ -45,8 +55,10 @@ const styles={
     }
 }
 
-const mapStateToProps = state =>{
-    return {selectedLibraryId: state.selectedLibraryId}
+const mapStateToProps = (state, ownProps) =>{
+    const expanded = state.selectedLibraryId === ownProps.library.item.id;
+
+    return {expanded};
 }
 
 export default connect(mapStateToProps, actions)(ListItem);
